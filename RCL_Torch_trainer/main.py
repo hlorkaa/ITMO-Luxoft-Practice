38d986d15b3a4dac5fc99bd50a6145109f2b34f0
Anton Kuznetsov <spessmehreene@gmail.com>
2021-06-17 20:59:36 +0800

    Updated code for both versions

--- RCL_Torch_trainer/main.py
+++ RCL_Torch_trainer/main.py
@@ -1,64 +1,83 @@
-#import torchvision.datasets as datasets
 import torch.nn as nn
 import torch
-import torch.nn.functional as F
-import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.utils import data
-
+from tqdm import tqdm
 
 TRAINING_DIR = 'data/training'
 trainset = datasets.ImageFolder(root=TRAINING_DIR, transform=transforms.ToTensor())
-trainloader = data.DataLoader(trainset, batch_size=40, shuffle=True)
+trainloader = data.DataLoader(trainset, batch_size=40)
 
 TESTING_DIR = 'data/training'
 testset = datasets.ImageFolder(root=TESTING_DIR, transform=transforms.ToTensor())
-testloader = data.DataLoader(testset, batch_size=40, shuffle=True)
+validloader = data.DataLoader(testset, batch_size=40) #testloader
 
 
-class NeuralNet(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+model = nn.Sequential(
+    nn.Conv2d(in_channels=3, out_channels=16, kernel_size=(3, 3)),
+    nn.ReLU(),
+    nn.MaxPool2d(kernel_size=(2, 2)),
+    nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 3)),
+    nn.ReLU(),
+    nn.MaxPool2d(kernel_size=(2, 2)),
+    nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3)),
+    nn.ReLU(),
+    nn.MaxPool2d(kernel_size=(2, 2)),
+    nn.Flatten(),
+    nn.Linear(in_features=69696, out_features=512),
+    nn.ReLU(),
+    nn.Linear(in_features=512, out_features=33),
+    nn.Softmax()
+)
 
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+print("Model is built!")
 
+# Declaring Criterion and Optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-model = NeuralNet()
-optimizer = optim.Adam(model.parameters())
-for(i, l) in trainloader:
-    optimizer.zero_grad()
-    output = model(i)
-    loss = F.nll_loss(output, l)
-    loss.backward()
-    optimizer.step()
+# Training with Validation
+epochs = 2
 
-model.eval()
-test_loss = 0
-correct = 0
-with torch.no_grad():
-    for data, target in testloader:
-        output = model(data)
-        test_loss += F.nll_loss(output, target, reduction='sum').item()
-        pred = output.argmax(dim=1, keepdim=True)
-        correct += pred.eq(target.view_as(pred)).sum().item()
+for e in range(epochs):
+    train_loss = 0.0
+    correct = 0.0
+    training_loop = tqdm(enumerate(trainloader), total=len(trainloader))
+    for batch_index, (data, labels) in training_loop:
+        # Transfer Data to GPU if available
+        if torch.cuda.is_available():
+            data, labels = data.cuda(), labels.cuda()
 
-test_loss /= len(testloader.dataset)
+        optimizer.zero_grad()  # Clear the gradients
 
-print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-    test_loss, correct, len(testloader.dataset),
-    100. * correct / len(testloader.dataset)))
+        target = model(data)  # Forward Pass
+        loss = criterion(target, labels)  # Find the Loss
+        loss.backward()  # Calculate gradients
+        optimizer.step()  # Update Weights
+        train_loss = loss.item() * data.size(0)  # Calculate Loss
 
-torch.save(model, 'torch_model.h5')
+        #Update progress bar
+        training_loop.set_description(f"Epoch [{e+1}/{epochs}], training progress")
+        training_loop.set_postfix(loss=train_loss/len(trainloader))
+
+    valid_loss = 0.0
+    model.eval()  # Optional when not using Model Specific layer
+    validation_loop = tqdm(enumerate(validloader), total=len(validloader))
+    for batch_index, (data, labels) in validation_loop:
+        # Transfer Data to GPU if available
+        if torch.cuda.is_available():
+            data, labels = data.cuda(), labels.cuda()
+
+        target = model(data)  # Forward Pass
+        loss = criterion(target, labels)  # Find the Loss
+        valid_loss = loss.item() * data.size(0)  # Calculate Loss
+
+        # Update progress bar
+        validation_loop.set_description(f"Epoch [{e + 1}/{epochs}], validation progress")
+        validation_loop.set_postfix(loss=valid_loss/len(validloader))
+
+        # Saving State Dict
+        # torch.save(model.state_dict(), 'torch_model.h5')
+
+print("Done!")
+torch.save(model.state_dict(), 'torch_model.h5')
